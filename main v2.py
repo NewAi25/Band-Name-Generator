5a98f10b1fe22d18c64a93a01e2e0a41de2fccb6
Manisha Sarkar <45327939+NewAi25@users.noreply.github.com>
2025-04-17 14:16:20 +0530

    Update main v2.py

--- main v2.py
+++ main v2.py
@@ -1,10 +1,10 @@
 # printing a welcome message for the user #
 print ("Welcome to the Band Name Generator")
 # Asking for user input #
-Colour= input("Enter your favourite colour: \n")
-Life = input("Enter your favourite Animal or flower or any living organism: \n")
+colour= input("Enter your favourite colour: \n")
+life = input("Enter your favourite Animal or flower or any living organism: \n")
 # capitalize the contents of two Variables #
-Band = Colour.capitalize()
-Name = Life.capitalize()
+band = colour.capitalize()
+name = life.capitalize()
 # print the final output after concatinating two strings #
-print ("Your Band Name is : " + Band + " " + Name)
+print ("Your Band Name is : " + band + " " + name)
