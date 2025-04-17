db39159676219bcaba2e66dd9eecaf618de019a2
Manisha Sarkar <45327939+NewAi25@users.noreply.github.com>
2025-04-17 11:50:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+print ("Welcome to the Band Name Generator")
 Band= input("Enter your favourite colour: ") + input("Enter your favourite Animal or flower or any living organism: ")
 Name= Band.capitalize()
 print (Name)
